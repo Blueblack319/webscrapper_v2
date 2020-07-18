fbba2d2df59588f8b1991b428c578ad9e0553211
hungry-eater <65373642+hungry-eater@users.noreply.github.com>
2020-07-18 12:12:09 +0900

    Almost done
    have to do exception handling

---
+++ day6.py
@@ -0,0 +1,89 @@
+import os
+import requests
+from bs4 import BeautifulSoup
+from babel.numbers import format_currency
+
+os.system("cls")
+
+"""
+Use the 'format_currency' function to format the output of the conversion
+format_currency(AMOUNT, CURRENCY_CODE, locale="ko_KR" (no need to change this one))
+"""
+
+# main, ask_from, ask_to, ask_money, convert
+def show_list():
+    url = "https://www.iban.com/currency-codes"
+    req = requests.get(url)
+    page = req.text
+    soup = BeautifulSoup(page, "html.parser")
+
+    table = soup.find("table")
+    rows = table.find_all("tr")[1:]
+    index = 0
+    countries = []
+
+    print("Welcome to CurrencyConvert PRO 2020")
+    for row in rows:
+        country_all = row.find_all("td")
+        name = country_all[0].get_text()
+        currency = country_all[2].get_text()
+        if currency != "No universal currency":
+            country = {name: currency}
+            countries.append(country)
+            print(f"#{index} {name}")
+            index += 1
+    return countries
+
+def search(countries):
+    index = int(input("#: "))
+    name = list(countries[index])[0]
+    currency = countries[index].get(name)
+    print(name)
+    return currency
+
+
+def ask_from(countries):
+    print("Where are you from? Choose a country by number.\n")
+    code = search(countries)
+    return  code
+
+def ask_to(countries):
+    print("Now choose another country.\n")
+    return search(countries)
+
+def ask_amount(currency_from, currency_to):
+    try:
+        amount = int(input((f"How many {currency_from} do you want to convert to {currency_to}?\n")))
+        return amount
+    except:
+        print("That wasn't a number.")
+        return ask_amount(currency_from, currency_to)
+
+def convert(code_from, code_to, source):
+    try:
+        url_trans = 'https://transferwise.com/gb/currency-converter/{}-to-{}-rate?amount={}'.format(code_from, code_to, source)
+        req = requests.get(url_trans)
+        page = req.text
+        soup = BeautifulSoup(page, "html.parser")
+
+        rate = float(soup.select_one("input#rate")["value"])
+        target = source*rate
+        amount_source = format_currency(source, code_from, locale="ko_KR")
+        amount_converted = format_currency(target, code_to, locale="ko_KR")
+
+        print(f"{amount_source} is {amount_converted}")
+
+    except:
+        print("Such coutries are not in transferwise...Sorry")
+
+def main():
+    countries = show_list()
+    code_from = ask_from(countries)
+    code_to = ask_to(countries)
+    print(code_from, code_to)
+    source = ask_amount(code_from, code_to)
+    convert(code_from, code_to, source)
+
+main()
+
+
