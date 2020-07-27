b5f545bc02ffa5eb8aa7eb3a11eabb25b20885ba
hungry-eater <65373642+hungry-eater@users.noreply.github.com>
2020-07-27 10:24:41 +0900

    Complete!

--- Day13~14/main.py
+++ Day13~14/main.py
@@ -8,19 +8,45 @@ https://remoteok.io/remote-dev+python-jobs
 Good luck!
 """
 
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, send_file, send_from_directory
+from werkzeug.utils import redirect
+
 from scrapper import extract_jobs
+from save import save_to_file
 
 app = Flask("Scrapping remote-job")
+db = {}
 
 @app.route("/")
 def home():
     return render_template("home.html")
 
+def check(term):
+    if term in db:
+        jobs = db[term]
+    else:
+        jobs = extract_jobs(term)
+        db[term] = jobs
+    return jobs
+
 @app.route("/search")
 def search():
-    term = request.args.get("term")
-    jobs = extract_jobs(term)
-    return render_template("search.html", jobs)
+    term = request.args.get("term").lower()
+    jobs = check(term)
+    number = len(jobs)
+    return render_template("search.html", jobs=jobs, number=number, term=term)
 
+@app.route("/export")
+def export():
+    try:
+        term = request.args.get("term").lower()
+        if not term:
+            raise Exception
+        jobs = db.get(term)
+        if not jobs:
+            raise Exception
+        save_to_file(term, jobs)
+        return send_file(f"remote_{term}_jobs.csv")
+    except:
+        return "lalalalal"
 app.run(host="localhost", port=5005)
---
+++ Day13~14/save.py
@@ -0,0 +1,8 @@
+import csv
+
+def save_to_file(term, jobs):
+    file = open(f"remote_{term}_jobs.csv", "w", -1, "utf-8")
+    writer = csv.writer(file)
+    writer.writerow(["Title", "Company", "Apply"])
+    for job in jobs:
+        writer.writerow(list(job.values()))
